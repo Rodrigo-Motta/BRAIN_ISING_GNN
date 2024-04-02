b3f9eb8c8ce54688645cfbcafc54d48c308f6b70
Rodrigo-Motta <rodrigodamottacc@gmail.com>
2024-04-02 10:43:37 -0300

    updated model

--- model.py
+++ model.py
@@ -10,16 +10,24 @@ class GCN(torch.nn.Module):
     def __init__(self,
                  num_features,
                  k_order,
-                 dropout=.5):
+                 dropout=.65):
         super(GCN, self).__init__()
 
         self.p = dropout
 
-        self.conv1 = ChebConv(int(num_features), 128, K=k_order)
-        self.conv2 = ChebConv(128, 128, K=k_order)
-        self.conv3 = ChebConv(128, 80, K=k_order)
+        # 190 ROIs
+        # self.conv1 = ChebConv(int(num_features), 128, K=k_order)
+        # self.conv2 = ChebConv(128, 128, K=k_order)
+        # self.conv3 = ChebConv(128, 64, K=k_order)
+        #
+        # self.lin1 = torch.nn.Linear(64, 16)
+        # self.lin2 = torch.nn.Linear(16, 1)
 
-        self.lin1 = torch.nn.Linear(80, 16)
+        # 333 ROIs
+        self.conv1 = ChebConv(int(num_features), 64, K=k_order)
+        self.conv2 = ChebConv(64, 32, K=k_order)
+
+        self.lin1 = torch.nn.Linear(32, 16)
         self.lin2 = torch.nn.Linear(16, 1)
 
         torch.nn.init.xavier_normal_(self.lin1.weight)
@@ -40,9 +48,10 @@ class GCN(torch.nn.Module):
             self.conv2(x, edge_index, edge_attr))  # WHY NAN WITH EDGE_ATTR (non-negative)
         x = func.dropout(x, p=self.p, training=self.training)
         # x = self.bn2(x)
-        x = torch.nn.functional.leaky_relu(
-            self.conv3(x, edge_index, edge_attr))  # , edge_attr, batch))  #WHY NAN WITH EDGE_ATTR (non-negative)
-        x = func.dropout(x, p=self.p, training=self.training)
+
+        #x = torch.nn.functional.leaky_relu(
+        #    self.conv3(x, edge_index, edge_attr))  # , edge_attr, batch))  #WHY NAN WITH EDGE_ATTR (non-negative)
+        #x = func.dropout(x, p=self.p, training=self.training)
 
         x = self.pool(x, batch)
         x1 = self.lin1(x)
